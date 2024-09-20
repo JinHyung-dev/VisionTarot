701fb01eda6ef8fb0fc6a5080eaf4da7c6204cd5
JinHyung-Park <trupjh@gmail.com>
2024-09-20 18:58:49 +0900

    메인 화면 정비, 뽑기 화면 연결 구현중

--- src/main/java/com/visiontarot/controller/MainController.java
+++ src/main/java/com/visiontarot/controller/MainController.java
@@ -5,11 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CardController {
+public class MainController {
 
-    @GetMapping("hello")
+    @GetMapping("/")
     public String index(Model model){
         model.addAttribute("data", "guest");
         return "index";
+    }
+    @GetMapping("/card/onecard")
+    public String onecard(Model model){
+        return "onecard";
+    }
+    @GetMapping("/card/threecard")
+    public String threecard(Model model) {
+        return "threecard";
     }
 }
