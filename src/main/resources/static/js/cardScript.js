fd5bdb78470e380a3e73b816bb2345e9ac4ee4aa
JinHyung-Park <trupjh@gmail.com>
2025-01-03 20:37:48 +0900

    feat(card.html, cardScript.js) : 1카드 뽑기 화면 출력 완료

--- src/main/resources/static/js/cardScript.js
+++ src/main/resources/static/js/cardScript.js
@@ -10,9 +10,31 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     document.getElementById("concernSection").style.display = 'none';
     document.getElementById("shuffleSection").style.display = 'block';
 
-    // 일정 시간 후 카드 선택 섹션 보여주기
+    //카드 뽑기
+    fetch('/card/onecard/draw')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const cardResult = document.getElementById('resultSection');
+            cardResult.innerHTML = `
+                        <p>카드명: ${data.cardName}</p>
+                        <p>카드종류: ${data.cardImgName}</p>
+                        <img src="${data.cardImgUrl}" alt="${data.cardName}"/>
+                    `;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    // 일정 시간 후 카드 결과 오픈
     setTimeout(function() {
         document.getElementById("shuffleSection").style.display = 'none';
-        document.getElementById("selectSection").style.display = 'block';
-    }, 3000); // 3초 후에 카드 선택 화면 보여주기
+        document.getElementById("resultSection").style.display = 'block';
+        // 카드 섞기
+        // document.getElementById("selectSection").style.display = 'block';
+    }, 3000); // 3초 후
 });
+
+function pickCard() {
+
+}
