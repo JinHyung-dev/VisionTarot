2b0d65fd3aada8031208e4d7596f46918bd3a5a6
JinHyung-Park <trupjh@gmail.com>
2025-02-03 03:09:35 +0900

    test(CardServiceTest) : 사용자 고민과 카드뽑기 결과에 대한 제미나이 해석 요청 단위 테스트 작성 및 완료

--- src/test/java/com/visiontarot/service/CardServiceTest.java
+++ src/test/java/com/visiontarot/service/CardServiceTest.java
@@ -1,26 +1,51 @@
 package com.visiontarot.service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import com.visiontarot.config.GeminiApiRequest;
 import com.visiontarot.repository.CardRepository;
+import java.util.HashMap;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CardServiceTest {
     private CardService service;
+    private final String SUMMARY = "할머니의 건강이 회복될 수 있을까요라는 고민에 대한 유니버셜 타로카드의 결과는 Nine of Swords 정방향입니다.";
+
+    @Mock
+    private GeminiApiRequest geminiApiRequest;
+
     @Autowired
     private CardRepository repository;
 
     @BeforeEach
     public void setUp() {
-        service = new CardService(repository);
+        service = new CardService(repository, geminiApiRequest);
     }
 
     @Test
     public void 원카드_뽑기() {
         assertNotNull(service.drawOneCard());
+    }
+
+    @Test
+    public void 제미나이_응답받기() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("Response Code", 200);
+        result.put("Response Body", "성공");
+
+        when(geminiApiRequest.makeRequest(SUMMARY)).thenReturn(result);
+        Assertions.assertThat(geminiApiRequest.makeRequest(SUMMARY)).isNotNull();
+        Assertions.assertThat(result.get("Response Code")).isEqualTo(200);
     }
 }
