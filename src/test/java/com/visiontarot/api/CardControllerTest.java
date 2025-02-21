3fc7bc20a51b399a6e8ce3042e22fea7480012d4
JinHyung-Park <trupjh@gmail.com>
2025-02-21 15:04:11 +0900

    test(CardControllerTest) : 카드 뽑기&제미나이 응답&고민카드 생성 및 업로드에 대한 "/onecard/draw-with-concerncard" post api 메서드 단위 테스트 작성 및 완료

--- src/test/java/com/visiontarot/api/CardControllerTest.java
+++ src/test/java/com/visiontarot/api/CardControllerTest.java
@@ -1,11 +1,17 @@
 package com.visiontarot.api;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.visiontarot.dto.CardDTO;
+import com.visiontarot.dto.GeminiResponseDTO;
 import com.visiontarot.service.CardService;
+import com.visiontarot.service.ConcernCardService;
+import java.awt.image.BufferedImage;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,9 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(CardController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -26,6 +32,9 @@ public class CardControllerTest {
 
     @MockBean
     private CardService cardService;
+
+    @MockBean
+    private ConcernCardService concernCardService;
 
     @BeforeEach
     public void setUp() {
@@ -42,7 +51,32 @@ public class CardControllerTest {
         mockMvc.perform(get("/card/onecard/draw"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.cardName")
+                .andExpect(jsonPath("$.cardName")
                         .value("The Fool"));
+    }
+
+    @Test
+    void 카드뽑고_제미나이응답_고민카드업로드_CardResponseDTO전송() throws Exception {
+        CardDTO card = new CardDTO(1L, "The Fool", "url", "imgName", "date", "date");
+        String concern = "예시 고민입니다.";
+        GeminiResponseDTO mockGeminiResponse = new GeminiResponseDTO("예시 응답입니다.", "완료", 200);
+        BufferedImage mockImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        String mockImageUrl = "https://s3.amazonaws.com/sample-image.png";
+
+        when(cardService.drawOneCard()).thenReturn(card);
+        when(cardService.getGeminiResponse(concern, card)).thenReturn(mockGeminiResponse);
+        when(concernCardService.createImage(any())).thenReturn(mockImage);
+        when(concernCardService.uploadImageToS3(any(BufferedImage.class))).thenReturn(mockImageUrl);
+
+        mockMvc.perform(post("/card/onecard/draw-with-concerncard").content(concern)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.card.cardName").value("The Fool"))
+                .andExpect(jsonPath("$.concern").value(concern))
+                .andExpect(jsonPath("$.geminiAnswer").value("예시 응답입니다."))
+                .andExpect(jsonPath("$.concernCardImageUrl").value(mockImageUrl));
+
+//        verify(concernCardService, times(1)).uploadImageToS3(any(BufferedImage.class));
     }
 }
