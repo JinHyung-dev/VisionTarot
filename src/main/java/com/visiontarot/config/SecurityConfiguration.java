dd2c5cacbecc06f260a519bf97467b3454bec7fe
JinHyung-Park <trupjh@gmail.com>
2025-02-21 18:45:55 +0900

    feat(SecurityConfiguration) : /user/** 엔드 포인트 제외 모두 접근 허용하도록 설정

--- src/main/java/com/visiontarot/config/SecurityConfiguration.java
+++ src/main/java/com/visiontarot/config/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,16 +14,18 @@ public class SecurityConfiguration {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/**").permitAll() // 모든 요청 허용
-                                .anyRequest().authenticated()
+                                .requestMatchers("/user/**").authenticated()
+                                .requestMatchers("/**").permitAll()
                 )
-                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (필요에 따라 설정)
                 .formLogin(formLogin ->
                         formLogin
-                                .loginPage("/login") // 로그인 페이지 설정
-                                .permitAll() // 모든 사용자가 로그인 페이지에 접근할 수 있도록 허용
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
                 );
 
         return http.build();
