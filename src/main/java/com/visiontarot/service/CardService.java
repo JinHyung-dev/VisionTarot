bdc5852759fb35a95c54b3e88499f668d69cd5dc
JinHyung-Park <trupjh@gmail.com>
2025-03-01 20:43:46 +0900

    refactor(CardService, CardServiceTest) : 제미나이 관련 로직 이동

--- src/main/java/com/visiontarot/service/CardService.java
+++ src/main/java/com/visiontarot/service/CardService.java
@@ -1,9 +1,7 @@
 package com.visiontarot.service;
 
-import com.visiontarot.config.GeminiApiRequestConfiguration;
 import com.visiontarot.domain.Card;
 import com.visiontarot.dto.CardDTO;
-import com.visiontarot.dto.GeminiResponseDTO;
 import com.visiontarot.repository.CardRepository;
 import java.util.List;
 import java.util.Random;
@@ -17,12 +15,10 @@ import org.springframework.stereotype.Service;
 public class CardService {
     private List<CardDTO> cardDTOS;
     private final CardRepository cardRepository;
-    private final GeminiApiRequestConfiguration geminiApiRequestConfiguration;
 
     @Autowired
-    public CardService(CardRepository cardRepository, GeminiApiRequestConfiguration geminiApiRequestConfiguration) {
+    public CardService(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
-        this.geminiApiRequestConfiguration = geminiApiRequestConfiguration;
     }
 
     public CardDTO drawOneCard() {
@@ -48,23 +44,5 @@ public class CardService {
         return cards.stream()
                 .map(Card::toDTO)  // 엔티티 -> DTO로 변환
                 .collect(Collectors.toList());
-    }
-
-    public GeminiResponseDTO getGeminiResponse(String concern, CardDTO card) {
-        String userConcernAndCardResult = createGeminiRequestString(concern, card);
-        log.info(">>> 다음 summary에 대해 제미나이 분석을 요청합니다.\n[{}]", userConcernAndCardResult);
-        return geminiApiRequestConfiguration.getGeminiResponse(userConcernAndCardResult);
-    }
-
-    private String createGeminiRequestString(String concern, CardDTO card) {
-        if (concern == null || card == null || card.getCardName() == null) {
-            return "잘못된 입력입니다. 고민 내용과 카드 정보를 다시 확인해주세요.";
-        }
-        log.info(">>> 고민과 카드뽑기 내용을 조합한 summary 생성");
-        return "현재 고민에 대한 유니버셜 타로카드 1카드 뽑기를 진행했어. \n"
-                + "고민: " + concern + "\n"
-                + "뽑기 결과: " + card.getCardName() + "카드가 "
-                + (card.isReversed() ? "역방향" : "정방향")
-                +"으로 나왔어. 한국말로 이 상황에서 이 카드가 무슨 의미일지 해석해줘.";
     }
 }
