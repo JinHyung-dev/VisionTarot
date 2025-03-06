01a8509749d31cab404b860cff218b6a750d4b28
JinHyung-Park <trupjh@gmail.com>
2025-03-06 18:52:03 +0900

    feat(cardScript.js) : 기능 변경에 따른 비동기 처리 로직 수정 및 고민카드 비동기 처리 추가, 요소 아이디 양식 일부 변경

--- src/main/resources/static/js/cardScript.js
+++ src/main/resources/static/js/cardScript.js
@@ -11,28 +11,32 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     document.getElementById("shuffleSection").style.display = 'block';
 
     //카드 뽑기
-    fetch('/card/onecard/draw')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            const cardResult = document.getElementById('result');
-            cardResult.innerHTML = `
-                        <p>카드명: ${data.cardName}</p>
-                        <p>카드종류: ${data.cardImgName}</p>
-                        <img src="${data.cardImgUrl}" alt="${data.cardName}" width="150px"/>
-                    `;
-
-            const reviewContent = document.getElementById('describe-content');
-            if(data.reversed) {
-               reviewContent.innerHTML = `"${concern}"라는 고민에 대한 유니버셜 타로카드의 결과는 ${data.cardName} 역방향입니다.`;
-            } else {
-               reviewContent.innerHTML = `"${concern}"라는 고민에 대한 유니버셜 타로카드의 결과는 ${data.cardName} 정방향입니다.`;
-            }
-            document.getElementById("describe").style.display = 'block';
+    fetch('/card/onecard/draw-with-analyze', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: concern
         })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+    .then(response => response.json())
+    .then(data => {
+        const cardResult = document.getElementById('result');
+        cardResult.innerHTML = `
+                    <p>카드명: ${data.card.cardName}</p>
+                    <p>카드종류: ${data.card.cardImgName}</p>
+                    <img src="${data.card.cardImgUrl}" alt="${data.card.cardName}" id="card-img" width="150px"/>
+                `;
+        const reviewContent = document.getElementById('describe-content');
+        if(data.card.reversed) {
+            reviewContent.innerText = `${concern}라는 고민에 대한 유니버셜 타로카드의 결과는 ${data.card.cardName} 역방향입니다.`;
+        } else {
+            reviewContent.innerText = `"${concern}"라는 고민에 대한 유니버셜 타로카드의 결과는 ${data.card.cardName} 정방향입니다.`;
+        }
+        reviewContent.innerText += ` ${data.geminiAnswer}`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
     // 일정 시간 후 카드 결과 오픈
     setTimeout(function() {
@@ -43,7 +47,7 @@ document.getElementById("submitBtn").addEventListener("click", function() {
     }, 3000); // 3초 후
 });
 
-document.getElementById("clipboardBtn").addEventListener("click", function () {
+document.getElementById("clipboard-btn").addEventListener("click", function () {
     const textToCopy = document.getElementById("describe-content").innerText;
 
     // 클립보드에 텍스트 복사
@@ -58,7 +62,25 @@ document.getElementById("clipboardBtn").addEventListener("click", function () {
         });
 });
 
+document.getElementById("check-btn").addEventListener("click", function() {
+    let summary = document.getElementById('describe-content').innerText;
 
-function pickCard() {
-
-}
+    fetch('/concern-card/create',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: summary
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const concernBox = document.getElementById('concern-card-box');
+            concernBox.innerHTML = `
+            <img src="${data.concernCardImageUrl}" alt="concern-card" id="concern-card-img" width="320px"/>
+        `;
+            document.getElementById("describe").style.display = 'block';
+        }).catch(error => {
+        console.error('Error:', error);
+    });
+});
